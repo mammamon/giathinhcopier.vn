53dc2a2d7651ebd7384cddc2048b66d24c514e05
mammamon <quangnguyenonlinevn2gmail.com>
2024-11-27 02:45:08 +0700

    header update

--- js/loader.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    $('#header-placeholder').load('../components/header.html');
-    $('#footer-placeholder').load('../components/footer.html');
-});
--- js/main.js
+++ js/main.js
@@ -1,24 +1,46 @@
-// Topbar visibility
-$(document).ready(function () {
-    let lastScrollTop = 0;
-    const topbarHeight = $('.topbar').outerHeight();
-    $(window).scroll(function () {
-        const st = $(this).scrollTop();
+//GLOBAl
+function loadComponent(selector, url, callback) {
+    if (!$(selector).children().length) {
+        $(selector).load(url, function() {
+            if (typeof callback === "function") callback();
+        });
+    } else {
+        if (typeof callback === "function") callback();
+    }
+}
+
+function loadHeader() {
+    // Function to manage header behavior
+    function headerBehavior() {
+        let lastScrollTop = 0;
         const topbar = $('.topbar');
         const navbar = $('.navbar');
-        if (st > lastScrollTop) {
-            topbar.css('top', `-${topbarHeight}px`);
-            navbar.css('top', '0');
-        } else {
-            topbar.css('top', '0');
-            navbar.css('top', `${topbarHeight}px`);
-        }
-        lastScrollTop = st <= 0 ? 0 : st;
-    });
+        const topbarHeight = topbar.outerHeight();
+
+        // Initialize topbar and navbar positions
+        topbar.css('transform', 'translateY(0)');
+        navbar.css('transform', `translateY(${topbarHeight}px)`);
+
+        $(window).scroll(function () {
+            const st = $(this).scrollTop();
+            if (st > lastScrollTop) {
+                topbar.css('transform', `translateY(-${topbarHeight}px)`);
+                navbar.css('transform', 'translateY(0)');
+            } else {
+                topbar.css('transform', 'translateY(0)');
+                navbar.css('transform', `translateY(${topbarHeight}px)`);
+            }
+            lastScrollTop = st <= 0 ? 0 : st;
+        });
+    }
+
+    // Load header component and initialize behavior
+    loadComponent('#header-placeholder', '../components/header.html', headerBehavior);
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    loadHeader();
+    loadComponent('#footer-placeholder', '../components/footer.html');
 });
-
-
-
-
-
 
