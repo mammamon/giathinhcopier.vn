591bfd6c3a03dcc2886c70a95e1dbd0e77df77ac
mammamon <quangnguyenonlinevn2gmail.com>
2024-11-27 08:50:07 +0700

    header improvement

--- js/main.js
+++ js/main.js
@@ -10,17 +10,13 @@ function loadComponent(selector, url, callback) {
 }
 
 function loadHeader() {
-    // Function to manage header behavior
     function headerBehavior() {
         let lastScrollTop = 0;
         const topbar = $('.topbar');
         const navbar = $('.navbar');
         const topbarHeight = topbar.outerHeight();
-
-        // Initialize topbar and navbar positions
         topbar.css('transform', 'translateY(0)');
         navbar.css('transform', `translateY(${topbarHeight}px)`);
-
         $(window).scroll(function () {
             const st = $(this).scrollTop();
             if (st > lastScrollTop) {
@@ -33,12 +29,10 @@ function loadHeader() {
             lastScrollTop = st <= 0 ? 0 : st;
         });
     }
-
-    // Load header component and initialize behavior
     loadComponent('#header-placeholder', '../components/header.html', headerBehavior);
 }
 
-
+// LOAD COMPONENTS
 document.addEventListener("DOMContentLoaded", function () {
     loadHeader();
     loadComponent('#footer-placeholder', '../components/footer.html');
